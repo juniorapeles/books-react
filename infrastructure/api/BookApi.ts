70c0f2bc5dd361da689a637b1d2c03b4809ded7f
eli <elijunior.apeles2005@gmail.com>
2025-06-15 21:02:57 -0300

    Fix new Base URL no http's'

--- infrastructure/api/BookApi.ts
+++ infrastructure/api/BookApi.ts
@@ -3,7 +3,7 @@ import { Book } from "@/domain/entities/Book";
 import { BookRepository } from "@/domain/repositories/BookRepository";
 
 const api = axios.create({
-  baseURL: "https://api.v1.juniorapeles.xyz",
+  baseURL: "http://api.v1.juniorapeles.xyz",
 });
 
 export class BookApi implements BookRepository {
